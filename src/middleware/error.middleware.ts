574b53a74541c7785fd9528e1912ce3631fd0f66
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-08-21 18:01:00 +0100

    Dev commit

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,17 +1,46 @@
 import type { Request, Response , NextFunction } from "express";
 import { AppError } from "../errors/app.error.js";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/client";
 
-export const errorHandler = (error: unknown, req: Request, res: Response, next: NextFunction) => {
-    console.error(error);
+export const errorHandler = (
+    err: unknown,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
 
-    if(error instanceof AppError) {
-        return res.status(error.statusCode).json({
-            message: error.message
-        })
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            message: err.message
+        });
     }
 
-    res.status(500).json({
-        message: "Internal Server error"
-    });
+    if (
+        err instanceof PrismaClientKnownRequestError
+    ) {
 
+        switch (err.code) {
+
+            case "P2002":
+                return res.status(409).json({
+                    message: "Emial already exist"
+                });
+
+            case "P2003":
+                return res.status(400).json({
+                    message: "Related record does not exist"
+                });
+
+            case "P2025":
+                return res.status(404).json({
+                    message: "Record not found"
+                });
+        }
+    }
+
+    console.error(err);
+
+    return res.status(500).json({
+        message: "Internal server error"
+    });
 };
